62a8e7f867ce9b39d8d5ab8bc19f6890b8a21519
analaura-gb <analauragbraga@gmail.com>
2024-11-12 15:50:28 -0300

    Layout de signIn e signUp

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import SignUp from "./pages/SignUp/SignUp";
+import SignIn from "./pages/SignIn/SignIn";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SignIn />,
+  },
+  {
+    path: "cadastro",
+    element: <SignUp />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ Front/src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid2 from "@mui/material/Grid2";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import FormControl from "@mui/material/FormControl";
+
+const SignUp = () => {
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#f0f2f5",
+      }}
+    >
+      <Paper
+        elevation={6}
+        sx={{
+          padding: 4,
+          borderRadius: 3,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "20%",
+          boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            mb: 3,
+          }}
+        >
+          <LockOutlinedIcon fontSize="large" color="primary" />
+          <Typography component="h1" variant="h5" sx={{ mt: 1 }}>
+            Sign Up
+          </Typography>
+        </Box>
+        <FormControl noValidate sx={{ width: "100%" }}>
+          <Grid2 container spacing={2}>
+            <Grid2 size={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid2>
+            <Grid2 size={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid2>
+          </Grid2>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            sx={{ mt: 3, mb: 2, py: 1.5 }}
+          >
+            Sign Up
+          </Button>
+          <Grid2 container justifyContent="flex-end">
+            <Grid2>
+              <Link href="cadastro" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid2>
+          </Grid2>
+        </FormControl>
+      </Paper>
+      <Box mt={5}></Box>
+    </Box>
+  );
+};
+
+export default SignUp;
